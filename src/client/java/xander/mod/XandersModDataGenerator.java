ae084a93d8c0bf665da29ab6e0981c5e1e76be65
Alex Christian <alexanderechristian@gmail.com>
2026-01-01 17:50:12 -0500

    Import fabric mod template

---
+++ src/client/java/xander/mod/XandersModDataGenerator.java
@@ -0,0 +1,11 @@
+package xander.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class XandersModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
