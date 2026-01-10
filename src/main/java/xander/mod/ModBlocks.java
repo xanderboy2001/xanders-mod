4a0884e65d061d40d0eac93606a1f1cd84625eb5
Alex Christian <alexanderechristian@gmail.com>
2026-01-09 20:12:30 -0500

    Generalized wall registration function

--- src/main/java/xander/mod/ModBlocks.java
+++ src/main/java/xander/mod/ModBlocks.java
@@ -18,18 +18,19 @@ import net.minecraft.util.Identifier;
 public class ModBlocks {
     public static final Map<Block, Block> LOG_TO_WALL = new HashMap<>();
 
-    public static Block registerLogWall(String name, Block baseLog) {
+    public static Block registerWall(String name, Block baseBlock) {
         Identifier id = Identifier.of("xander", name + "_wall");
         RegistryKey<Block> blockKey = RegistryKey.of(Registries.BLOCK.getKey(), id);
         RegistryKey<Item> itemKey = RegistryKey.of(Registries.ITEM.getKey(), id);
 
+        BlockSoundGroup soundGroup = baseBlock.getDefaultState().getSoundGroup();
+
         Block wall = new WallBlock(
                 AbstractBlock.Settings
-                        .copy(Blocks.COBBLESTONE_WALL)
+                        .copy(baseBlock)
                         .registryKey(blockKey)
-                        .strength(baseLog.getHardness(), baseLog.getBlastResistance())
-                        .sounds(BlockSoundGroup.WOOD)
-                        .burnable());
+                        .strength(baseBlock.getHardness(), baseBlock.getBlastResistance())
+                        .sounds(soundGroup));
         Registry.register(Registries.BLOCK, id, wall);
 
         Registry.register(
@@ -37,19 +38,19 @@ public class ModBlocks {
                 itemKey,
                 new BlockItem(wall, new Item.Settings().registryKey(itemKey)));
 
-        LOG_TO_WALL.put(baseLog, wall);
+        LOG_TO_WALL.put(baseBlock, wall);
         return wall;
     }
 
     public static void registerWalls() {
-        registerLogWall("oak", Blocks.OAK_LOG);
-        registerLogWall("spruce", Blocks.SPRUCE_LOG);
-        registerLogWall("birch", Blocks.BIRCH_LOG);
-        registerLogWall("jungle", Blocks.JUNGLE_LOG);
-        registerLogWall("acacia", Blocks.ACACIA_LOG);
-        registerLogWall("dark_oak", Blocks.DARK_OAK_LOG);
-        registerLogWall("mangrove", Blocks.MANGROVE_LOG);
-        registerLogWall("cherry", Blocks.CHERRY_LOG);
-        registerLogWall("pale_oak", Blocks.PALE_OAK_LOG);
+        registerWall("oak", Blocks.OAK_LOG);
+        registerWall("spruce", Blocks.SPRUCE_LOG);
+        registerWall("birch", Blocks.BIRCH_LOG);
+        registerWall("jungle", Blocks.JUNGLE_LOG);
+        registerWall("acacia", Blocks.ACACIA_LOG);
+        registerWall("dark_oak", Blocks.DARK_OAK_LOG);
+        registerWall("mangrove", Blocks.MANGROVE_LOG);
+        registerWall("cherry", Blocks.CHERRY_LOG);
+        registerWall("pale_oak", Blocks.PALE_OAK_LOG);
     }
 }
