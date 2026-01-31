f30011a1a8e32698f4cdb33f79d4554f3f848fd7
Alex Christian <alexanderechristian@gmail.com>
2026-01-30 19:26:23 -0500

    Moved namespace to 'xander'

--- src/main/java/xander/mod/XandersMod.java
+++ src/main/java/xander/mod/XandersMod.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class XandersMod implements ModInitializer {
-	public static final String MOD_ID = "xanders-mod";
+	public static final String MOD_ID = "xander";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
